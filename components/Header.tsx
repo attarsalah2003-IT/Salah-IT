a3d93360f91abb4b7474532d5407523c6b5ad2cd
shadowcracks <yst187business@gmail.com>
2024-10-02 22:04:13 -0400

    modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
-
 import React from "react";
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  navItems: string[];
+}
+
+const Header: React.FC<HeaderProps> = ({ navItems }) => {
   return (
     <header className="flex flex-col justify-center items-center px-16 py-3 w-full text-base text-white border border-black border-solid bg-neutral-900 max-md:px-5 max-md:max-w-full">
       <div className="flex flex-wrap gap-5 justify-between w-full max-w-[1157px] max-md:max-w-full">
@@ -12,11 +15,9 @@ const Header: React.FC = () => {
           className="object-contain shrink-0 aspect-square w-[71px]"
         />
         <nav className="flex flex-wrap gap-10 items-start self-start mt-5 max-md:max-w-full">
-          <a href="Home">Home</a>
-          <a href="About">About</a>
-          <a href="Services">Services</a>
-          <a href="Pricing">Pricing</a>
-          <a href="blog">Blog</a>
+          {navItems.map((item, index) => (
+            <a key={index} href={item.replace(/\s+/g, '')}>{item}</a>
+          ))}
           <a
             href="ContactUs"
             className="self-stretch px-8 py-2.5 text-blue-400 border border-blue-400 border-solid rounded-[100px] max-md:px-5"
